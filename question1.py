f1642e214b43bd809fb578ce7c6d5d1dd671a359
TwixxOp15 <suhairrizwanofficial@gmail.com>
2026-01-27 23:49:27 +0500

    Added solution for Question 1 part (c)(ii)

--- question1.py
+++ question1.py
@@ -30,45 +30,49 @@ def outputNodes(linkedList, startPointer):
         print(linkedList[startPointer].data)
         startPointer = linkedList[startPointer].nextNode
 
-
-# d.i.
-def addNode(linkedList, startPointer, emptyList):
-    success = False
-    data = int(input("Enter data to add: "))
-
-    # Getting end of the list:
-    while startPointer != -1:
-        prevPointer = startPointer
-        startPointer = linkedList[startPointer].nextNode
-
-    while linkedList[emptyList].nextNode != -1:
-        # New Node:
-        linkedList[emptyList].data = data
-        linkedList[emptyList].nextNode = -1
-
-        # updating to join th new Node in linked list
-        linkedList[prevPointer].nextNode = emptyList
-
-        # Empty List updating
-        emptyList = linkedList[emptyList].nextNode
-        success = True
-
-        # Return true if inserted successfully
-        return success
-
-    # List is full as no space in emptyList
-    return success
-
-
 # c.ii.
-# d.ii
-def main():
-    outputNodes(linkedList, startPointer)
-    if addNode(linkedList, startPointer, emptyList):
-        print("Successfully added node")
-    else:
-        print("No space left to add node")
-    outputNodes(linkedList, startPointer)
+
+outputNodes(linkedList, startPointer)
 
 
-main()
+# # # d.i.
+# # def addNode(linkedList, startPointer, emptyList):
+# #     success = False
+# #     data = int(input("Enter data to add: "))
+
+# #     # Getting end of the list:
+# #     while startPointer != -1:
+# #         prevPointer = startPointer
+# #         startPointer = linkedList[startPointer].nextNode
+
+# #     while linkedList[emptyList].nextNode != -1:
+# #         # New Node:
+# #         linkedList[emptyList].data = data
+# #         linkedList[emptyList].nextNode = -1
+
+# #         # updating to join th new Node in linked list
+# #         linkedList[prevPointer].nextNode = emptyList
+
+# #         # Empty List updating
+# #         emptyList = linkedList[emptyList].nextNode
+# #         success = True
+
+# #         # Return true if inserted successfully
+# #         return success
+
+# #     # List is full as no space in emptyList
+# #     return success
+
+
+
+# # # d.ii
+# # def main():
+# #     outputNodes(linkedList, startPointer)
+# #     if addNode(linkedList, startPointer, emptyList):
+# #         print("Successfully added node")
+# #     else:
+# #         print("No space left to add node")
+# #     outputNodes(linkedList, startPointer)
+
+
+# main()
